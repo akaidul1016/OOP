92d5b254a96fbefbd1b3eeb6fe869ae7a53f9f23
Akaidul Islam <akaidulislam1016@gmail.com>
2026-08-02 05:17:20 +0600

    commit

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner ineeput = new Scanner(System.in);
         double name;
         System.out.println("Enter your name: ");
-        name= input.nextDouble();
+        name= ineeput.nextDouble();
         System.out.println("Hello " + name);
     
-        
+         
     }
 }
